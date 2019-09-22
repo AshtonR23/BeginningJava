1150c448c5d277af4a9b864bf290f02c8136d72c
ashtonrischer <rischera23@greenhill.org>
2019-09-22 18:32:00 -0500

    zdfh

---
+++ src/Lanterns.java
@@ -0,0 +1,54 @@
+/* Ashton Rischer
+9/22/19
+This will be a program that prints out a Lantern shape
+ */
+public class Lanterns {
+
+    public static final int SIZE = 4;
+
+    public static void main(String[] args) {
+        lantern();
+        lantern2();
+        lantern3();
+        row();
+        slashrow();
+    }
+
+    public static void lantern()
+    {
+       System.out.println("    *****");
+       System.out.println("  *********");
+       System.out.println("*************");
+       System.out.println("             ");
+    }
+    public static void lantern2()
+    {
+        System.out.println("    *****");
+        System.out.println("  *********");
+        System.out.println("*************");
+    }
+
+    public static void slashrow()
+    {
+        System.out.println("* | | | | | *");
+    }
+    public static void row()
+    {
+        System.out.println("*************");
+    }
+    public static void lantern3()
+
+{
+    System.out.println("    *****");
+    System.out.println("  *********");
+    System.out.println("*************");
+}
+
+
+
+}
+
+
+
+
+
